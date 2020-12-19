93124cf25a53a42c8889cb7087c40435759d843e
Patrick Menendez <patrickmenendez29@gmail.com>
2020-12-18 19:29:45 -0600

    initial commit, already working

--- Sources/StorageKit/StorageKit.swift
+++ Sources/StorageKit/StorageKit.swift
@@ -1,3 +1,125 @@
-struct StorageKit {
-    var text = "Hello, World!"
+import SwiftUI
+import Foundation
+
+class Entity: Codable{
+    
+   public init(id: String, name: String, description: String) {
+        self.id = id
+        self.name = name
+        self.description = description
+    }
+    
+    
+    var id: String
+    var name: String
+    var description: String
+    
+    
+}
+
+
+class Container {
+    
+    internal init(containerName: String) {
+        self.containerName = containerName
+        self.entities = [:]
+        
+        let directoryURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        
+        let newContainerPath = directoryURL.appendingPathComponent(self.containerName)
+        
+        // Have not checked if directory gets destroyed every time (it should not)
+        do {
+            try FileManager.default.createDirectory(at: newContainerPath, withIntermediateDirectories: true, attributes: nil)
+        } catch let error as NSError {
+            print("Unable to create directory \(error.debugDescription)")
+        }
+        print("New directory: \(newContainerPath.path)")
+    }
+    
+    let containerName: String
+    var entities: [String: Entity]
+    
+    func appendEntity(entity: Entity){
+        
+        self.entities[entity.id] = entity
+        let jsonEncoder = JSONEncoder()
+        do {
+            
+            let jsonData = try jsonEncoder.encode(entity)
+
+            
+            let directoryURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+            
+            let newContainerPath = directoryURL.appendingPathComponent(self.containerName)
+            
+            let newEntityPath = newContainerPath.appendingPathComponent("\(entity.id).json")
+            
+            try jsonData.write(to: newEntityPath)
+            print("Entity saved at: \(newEntityPath.absoluteURL)")
+            
+        } catch {
+            print("error saving entity")
+        }
+     
+       
+    }
+    
+    func removeEntity(entity: Entity){
+        
+        let directoryURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        
+        let newContainerPath = directoryURL.appendingPathComponent(self.containerName)
+        
+        let newEntityPath = newContainerPath.appendingPathComponent(entity.id)
+        
+        do {
+            try FileManager.default.removeItem(at: newEntityPath)
+
+            print("Entity deleted")
+            
+        } catch {
+            print("Error deleting entity")
+        }
+        
+    }
+    
+    func fetchEntities() -> [Entity]{
+        
+        let fileManager = FileManager.default
+        
+        let directoryURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0]
+        
+        let newContainerPath = directoryURL.appendingPathComponent(self.containerName)
+        
+        var entities: [Entity] = []
+        
+        do {
+            let fileURLs = try fileManager.contentsOfDirectory(at: newContainerPath, includingPropertiesForKeys: nil)
+            // process files
+            
+            for file in fileURLs {
+                
+                do {
+                    
+                    let jsonData = try Data(contentsOf: file)
+                    
+                 
+                    
+                    
+                    let newEntity: Entity  = try JSONDecoder().decode(Entity.self, from: jsonData as Data)
+                    print(newEntity)
+                    entities.append(newEntity)
+                } catch {
+                    print("Error reading entity at file \(file.absoluteString)")
+                    print(error.localizedDescription)
+                }
+            }
+        } catch {
+            print("Error while enumerating files \(newContainerPath.path): \(error.localizedDescription)")
+        }
+        
+        return entities
+    }
+    
 }
--- Tests/StorageKitTests/StorageKitTests.swift
+++ Tests/StorageKitTests/StorageKitTests.swift
@@ -6,7 +6,15 @@ final class StorageKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(StorageKit().text, "Hello, World!")
+        //XCTAssertEqual(StorageKit, "Hello, World!")
+        var entity = Entity(id: "Test Entity", name: "John", description: "A simple user")
+        var container = Container(containerName: "User Container")
+        
+        container.appendEntity(entity: entity)
+        
+        var entities = container.fetchEntities()
+        
+        print(entities[0].name)
     }
 
     static var allTests = [
